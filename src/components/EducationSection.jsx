e75a4d384a3f65ce186f3ed8c49f275f043812a5
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-05-21 08:20:16 -0400

    Fix layout for education

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -13,53 +13,52 @@ export const EducationSection = () => {
 						<span className="text-primary">Education</span>
 					</h2>
 				</div>
-			</div>
-
-			{/* Experience Timeline */}
-			<div className="relative">
-				{/* Education Entries */}
-				{education.map((edu) => (
-					<div
-						key={edu.id}
-						className={`flex flex-col sm:flex-row items-center mb-16`}
-					>
-						{/* Content Section */}
+				{/* Experience Timeline */}
+				<div className="relative">
+					{/* Education Entries */}
+					{education.map((edu) => (
 						<div
-							className={`w-full p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md sm:ml-44 sm:mr-44 ml-8 transform transition-transform duration-300 hover:scale-105`}
+							key={edu.id}
+							className={`flex flex-col sm:flex-row items-center mb-16`}
 						>
-							{/* Flex container for image and text */}
-							<div className="flex items-center justify-between">
-								{/* Degree, School Name, Dates */}
-								<div className="relative">
-									<div className="flex flex-wrap items-center justify-between">
-										<img
-											className="w-12 h-12 mr-4 rounded-full object-fitr"
-											src={edu.img}
-											alt={edu.school}
-										/>
-										<h3 className="text-xl sm:text-xl font-semibold text-white">
-											{edu.degree}
-										</h3>
-										<h4 className="mx-2 text-xl sm:text-xl font-semibold text-gray-300">
-											| {edu.school}
-										</h4>
-									</div>
+							{/* Content Section */}
+							<div
+								className={`w-full p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md sm:ml-44 sm:mr-44 ml-8 transform transition-transform duration-300 hover:scale-105`}
+							>
+								{/* Flex container for image and text */}
+								<div className="flex items-center justify-between">
+									{/* Degree, School Name, Dates */}
+									<div className="relative">
+										<div className="flex flex-wrap items-center justify-between">
+											<img
+												className="w-12 h-12 mr-4 rounded-full object-fitr"
+												src={edu.img}
+												alt={edu.school}
+											/>
+											<h3 className="text-xl sm:text-xl font-semibold text-white">
+												{edu.degree}
+											</h3>
+											<h4 className="mx-2 text-xl sm:text-xl font-semibold text-gray-300">
+												| {edu.school}
+											</h4>
+										</div>
 
-									{/* Date at the bottom */}
-									<div className="flex items-center mt-2 text-sm text-gray-400 font-bold text-left">
-										<p>{edu.date}</p>
-										<p className="px-2">
-											| Grade: {edu.grade}
-										</p>
+										{/* Date at the bottom */}
+										<div className="flex items-center mt-2 text-sm text-gray-400 font-bold text-left">
+											<p>{edu.date}</p>
+											<p className="px-2">
+												| Grade: {edu.grade}
+											</p>
+										</div>
 									</div>
 								</div>
+								<p className="mt-4 text-gray-400 text-left">
+									{edu.desc}
+								</p>
 							</div>
-							<p className="mt-4 text-gray-400 text-left">
-								{edu.desc}
-							</p>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</section>
 	);
